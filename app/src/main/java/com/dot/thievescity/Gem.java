bf3309c1ded2b74e212df9bf387cb75b603f997b
sanathbharadwaj <sanath.bharadwaj21@gmail.com>
2017-10-06 18:05:46 +0530

    Added modules

---
+++ app/src/main/java/com/dot/thievescity/Gem.java
@@ -0,0 +1,12 @@
+package com.dot.thievescity;
+
+/**
+ * Created by Admin on 10/6/2017.
+ */
+
+public class Gem {
+    public int type = 0;
+    public Gem(int type){
+        this.type = type;
+    }
+}
--- app/src/main/java/com/dot/thievescity/MainActivity.java
+++ app/src/main/java/com/dot/thievescity/MainActivity.java
@@ -1,14 +1,22 @@
+//This is the activity for the login screen
+
 package com.dot.thievescity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void onSubmit(View view){
+        //Handle operations when user clicks submit button
+    }
+
 }
 
