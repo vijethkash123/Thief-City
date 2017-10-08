68f3e1237b04c0093de42ef855fd714984e18f34
vijethkash123 <vijethkashyap035@gmail.com>
2017-10-08 13:01:12 +0530

    Hizzzzzzzzzzzzz

--- app/src/main/java/com/dot/thievescity/GamePlayActivity.java
+++ app/src/main/java/com/dot/thievescity/GamePlayActivity.java
@@ -1,18 +1,35 @@
 package com.dot.thievescity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.dot.thievescity.utils.GpsTracker;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class GamePlayActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    LocationManager locationManager;
+    LocationListener locationListener;
+    GpsTracker gps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +39,28 @@ public class GamePlayActivity extends FragmentActivity implements OnMapReadyCall
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -37,18 +74,64 @@ public class GamePlayActivity extends FragmentActivity implements OnMapReadyCall
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng silver = new LatLng(13.022775, 76.102263);
+        mMap.addMarker(new MarkerOptions().position(silver).title("Marker in Silver Jubliee").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(silver,15));
+
+        LatLng home1 = new LatLng(13.023611, 76.102263);
+        mMap.addMarker(new MarkerOptions().position(home1).title("Marker in Main Gate").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(home1,15));
+
+        LatLng stage = new LatLng(13.024427, 76.103561);
+        mMap.addMarker(new MarkerOptions().position(stage).title("Marker in Stage"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(stage,15));
+        gps=new GpsTracker(this, mMap);
+
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+
+
+        }
+        else{
+            Location lastKnownLocation =null;
+            if(gps.canGetLocation())
+            {
+                lastKnownLocation = gps.getLocation();
+                LatLng yourLoc = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(yourLoc).title("Marker in User Location").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(yourLoc,15));
+
+                Location loc1 = gps.getLocation();
+                Location loc2 = gps.getLocation();
+
+                float distanceInMeters = loc1.distanceTo(loc2);
+
+                Toast.makeText(this,distanceInMeters+"",Toast.LENGTH_LONG).show();
+            }
+            else Toast.makeText(this,"Can't get location",Toast.LENGTH_SHORT).show();
+
+
+        }
+
+
+
     }
+
 
     void loadGems(){
 
     }
 
     void markGems(){
+
+    }
+
+    void loadOtherGems(){
 
     }
 
@@ -79,3 +162,4 @@ public class GamePlayActivity extends FragmentActivity implements OnMapReadyCall
 
 
 }
+
---
+++ app/src/main/java/com/dot/thievescity/utils/GpsTracker.java
@@ -0,0 +1,234 @@
+package com.dot.thievescity.utils;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Service;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+/**
+ * Ahmet Ertugrul OZCAN
+ * Cihazin konum bilgisini goruntuler
+ */
+public class GpsTracker extends Service implements LocationListener {
+    private final Context mContext;
+    GoogleMap mMap;
+    // Cihazda gps acik mi?
+    boolean isGPSEnabled = false;
+
+    // Cihazda veri baglantisi aktif mi?
+    boolean isNetworkEnabled = false;
+
+    boolean canGetLocation = false;
+
+    // Konum
+    Location location;
+    // Enlem
+    double latitude;
+    // Boylam
+    double longitude;
+
+    // Konum guncellemesi gerektirecek minimum degisim miktari
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // metre
+
+    // Konum guncellemesi gerektirecek minimum sure miktari
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // dakika
+
+    // LocationManager nesnesi
+    protected LocationManager locationManager;
+
+    //
+    // Kurucu Metod - Constructor
+    //
+    public GpsTracker(Context context, GoogleMap map) {
+        this.mContext = context;
+        this.mMap=map;
+        getLocation();
+    }
+
+    //
+    // Konum bilgisini dondurur
+    //
+    public Location getLocation() {
+        try {
+            locationManager = (LocationManager) mContext.getSystemService(LOCATION_SERVICE);
+            // GPS acik mi?
+            isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            // Internet acik mi?
+            isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            if (!isGPSEnabled && !isNetworkEnabled) {
+
+            } else {
+                this.canGetLocation = true;
+
+                // Once internetten alinan konum bilgisi kayitlanir
+                if (isNetworkEnabled) {
+                    if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        Log.d("GPS", "not permitted");
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return null;
+                    }
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                    Log.d("Network", "Network");
+                    if (locationManager != null)
+                    {
+                        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        if (location != null)
+                        {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                        }
+                    }
+                }
+                // GPS'ten alinan konum bilgisi;
+                if (isGPSEnabled)
+                {
+                    if (location == null)
+                    {
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                        Log.d("GPS Enabled", "GPS Enabled");
+                        if (locationManager != null)
+                        {
+                            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            if (location != null)
+                            {
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        return location;
+    }
+
+    // Enlem bilgisini dondurur
+    public double getLatitude()
+    {
+        if(location != null)
+        {
+            latitude = location.getLatitude();
+        }
+
+        return latitude;
+    }
+
+    // Boylam bilgisini dondurur
+    public double getLongitude()
+    {
+        if(location != null)
+        {
+            longitude = location.getLongitude();
+        }
+
+        return longitude;
+    }
+
+    @Override
+    public void onLocationChanged(Location location)
+    {
+        LatLng yourLoc = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(yourLoc).title("Marker in User Location").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(yourLoc,15));
+    }
+
+    @Override
+    public void onProviderDisabled(String provider)
+    {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider)
+    {
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras)
+    {
+    }
+
+    @Override
+    public IBinder onBind(Intent arg0)
+    {
+        return null;
+    }
+
+    public boolean canGetLocation()
+    {
+        return this.canGetLocation;
+    }
+
+    // Konum bilgisi kapali ise kullaniciya ayarlar sayfasina baglanti iceren bir mesaj goruntulenir
+    public void showSettingsAlert()
+    {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
+
+        // Mesaj basligi
+        alertDialog.setTitle("GPS Kapalı");
+
+        // Mesaj
+        alertDialog.setMessage("Konum bilgisi alınamıyor. Ayarlara giderek gps'i aktif hale getiriniz.");
+
+        // Mesaj ikonu
+        //alertDialog.setIcon(R.drawable.delete);
+
+        // Ayarlar butonuna tiklandiginda
+        alertDialog.setPositiveButton("Ayarlar", new DialogInterface.OnClickListener()
+        {
+            public void onClick(DialogInterface dialog,int which)
+            {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                mContext.startActivity(intent);
+            }
+        });
+
+        // Iptal butonuna tiklandiginda
+        alertDialog.setNegativeButton("İptal", new DialogInterface.OnClickListener()
+        {
+            public void onClick(DialogInterface dialog, int which)
+            {
+                dialog.cancel();
+            }
+        });
+
+        // Mesaj kutusunu goster
+        alertDialog.show();
+    }
+
+    // LocationManager'in gps isteklerini durdurur
+    public void stopUsingGPS()
+    {
+        if(locationManager != null)
+        {
+            locationManager.removeUpdates(GpsTracker.this);
+        }
+    }
+}
